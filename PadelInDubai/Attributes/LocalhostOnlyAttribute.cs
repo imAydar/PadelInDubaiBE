14218de1ab287b6a8087c03c92d7194e09531a69
Aydar <aydar.g@msp360.com>
2025-04-15 02:29:53 +0400

    localhost only attr

--- PadelInDubai/Attributes/LocalhostOnlyAttribute.cs
+++ PadelInDubai/Attributes/LocalhostOnlyAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace PadelInDubai.Attributes
 {
@@ -8,11 +9,15 @@ namespace PadelInDubai.Attributes
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             var remoteIp = context.HttpContext.Connection.RemoteIpAddress;
-            var localIp = context.HttpContext.Connection.LocalIpAddress;
+            //var localIp = context.HttpContext.Connection.LocalIpAddress;
 
-            if (remoteIp?.ToString() != "127.0.0.1" &&
-                remoteIp?.ToString() != "::1" &&
-                remoteIp?.ToString() != localIp?.ToString())
+            //if (remoteIp?.ToString() != "127.0.0.1" &&
+            //    remoteIp?.ToString() != "::1" &&
+            //    remoteIp?.ToString() != localIp?.ToString())
+            //{
+            //    context.Result = new StatusCodeResult(403);
+            //}
+            if (!IPAddress.IsLoopback(remoteIp))
             {
                 context.Result = new StatusCodeResult(403);
             }
